ceca96bb230e7c2ae9a02c2141ea1e9e5432669a
Gordon Xie <z5160618@student.unsw.edu.au>
2020-06-14 19:38:07 +1000

    created basic sql tables

--- backend/db_data/db.sql
+++ backend/db_data/db.sql
@@ -1,1 +1,65 @@
 -- sql file for database schema
+
+-- Create candidate table
+CREATE TABLE Candidate (
+	first_name TEXT NOT NULL,
+	last_name TEXT NOT NULL,
+	email TEXT PRIMARY KEY,
+	university TEXT,
+	password TEXT NOT NULL
+);
+
+-- Create ePortfolioLink table
+CREATE TABLE ePortfolioLink (
+	link TEXT PRIMARY KEY
+);
+
+-- Create Employer table
+CREATE TABLE Employer (
+	name TEXT NOT NULL,
+	email TEXT PRIMARY KEY,
+	graduateCriteria TEXT,
+	skillsCriteria TEXT,
+	password TEXT NOT NULL
+);
+
+-- Create Course Admin table
+CREATE TABLE CourseAdmin (
+	email TEXT PRIMARY KEY,
+	university TEXT NOT NULL,
+	password TEXT NOT NULL
+);
+
+-- Create SkillsBackpackAdmin table
+CREATE TABLE SkillsBackpackAdmin (
+	email TEXT PRIMARY KEY,
+	password TEXT
+);
+
+-- Create Course table
+CREATE TABLE Course (
+	code TEXT NOT NULL,
+	learningOutcomes TEXT,
+	university TEXT NOT NULL,
+	faculty TEXT NOT NULL,
+	gradOutcomes TEXT,
+	description TEXT,
+	name TEXT NOT NULL,
+	PRIMARY KEY(code, university)
+);
+
+-- Create ePortfolio table
+CREATE TABLE ePortfolio(
+	name TEXT PRIMARY KEY,
+	employabilitySkills TEXT, 
+	jobSkills TEXT
+);
+
+-- Create Employment table
+CREATE TABLE Employment (
+	id INTEGER PRIMARY KEY,
+	description TEXT,
+	startDate TEXT NOT NULL,
+	endDate TEXT NOT NULL,
+	employer TEXT NOT NULL,
+);
