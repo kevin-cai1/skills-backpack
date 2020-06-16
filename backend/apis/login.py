f23c7ca66a3dbbeea0317aadfb5ff6039fb094ac
Kevin Cai <kevincai222@gmail.com>
2020-06-17 00:38:47 +1000

    adding outline for creating account

--- backend/apis/login.py
+++ backend/apis/login.py
@@ -4,8 +4,15 @@ from flask import request, jsonify
 api = Namespace('Login', description='Login validation operations')
 
 login_package = api.model('login', {
-    'username' : fields.String(description='unique user name for account identification'),
-    'password': fields.String(description='password for account access')
+    'email' : fields.String(description='university email for account identification', required=True),
+    'password'  : fields.String(description='password for account access', required=True)
+})
+
+account_package = api.model('create', {
+    'email' : fields.String(description='university email for account identification', required=True),
+    'password'  : fields.String(description='password for account access', required=True),
+    'name' : fields.String(description='name of user', required=True),
+    'user_type' : fields.String(description='access type of the user', required=True)
 })
 
 @api.route('/validate')
@@ -13,7 +20,7 @@ class login(Resource):
     @api.expect(login_package)
     def post(self):
         req = request.get_json()
-        print(req['username'])
+        print(req['email'])
         print(req['password'])
         # check for matching username listing in db
         # if not in database
@@ -27,3 +34,14 @@ class login(Resource):
             # give OK response
         return 0
 
+@api.route('/create')
+class createAccount(Resource):
+    @api.expect(account_package)
+    def post(self):
+        req = request.get_json()
+        # if user already exists
+            # api.abort(400, "User '{}' already exists".format(req['username']))
+
+        # check validity of fields? (strings, integer, etc)
+
+        # 
