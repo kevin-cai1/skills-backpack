2bc95a7db67214239084643ffd76d52f7a631efa
Kevin Cai <kevincai222@gmail.com>
2020-06-24 15:42:50 +1000

    initial email invite complete

--- backend/apis/__init__.py
+++ backend/apis/__init__.py
@@ -4,6 +4,7 @@ from .test import api as ns1
 from .account import api as ns2
 from .candidate import api as ns3
 from .skillsAdmin import api as ns4
+from .courseAdmin import api as ns5
 
 
 api = Api(
@@ -16,3 +17,4 @@ api.add_namespace(ns1, path='/test')
 api.add_namespace(ns2, path='/account')
 api.add_namespace(ns3, path='/candidate')
 api.add_namespace(ns4, path='/skills_admin')
+api.add_namespace(ns5, path='/course_admin')
---
+++ backend/apis/courseAdmin.py
@@ -0,0 +1,51 @@
+from flask_restplus import Namespace, Resource, fields
+from flask import request, jsonify
+
+import db
+import os
+
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+api = Namespace('Course Admin', description='Course Admin invite and creation')
+
+email_details = api.model('update', {
+    'skills_email'  : fields.String(description='email of skills admin inviting course admin', required=True),
+    'course_email' : fields.String(description='new password for account access', required=True)
+})
+
+@api.route('/invite')
+class Invite(Resource):
+    @api.doc(description="sends an email invite to the specified email")
+    @api.expect(email_details)
+    def post(self):
+        req = request.get_json()
+        skills_email = req['skills_email']
+        course_email = req['course_email']
+
+        message = Mail(
+            from_email=skills_email,
+            to_emails=course_email,
+            subject='Join Skills Backpack platform!',
+            html_content='<h1>Welcome {}</h1><strong> {} has invited you to join Skills Backpack!</strong><br><br><a href="http://localhost:3000/register">Click here</a> to join'.format(course_email, skills_email)
+        )
+
+        try:
+            sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+            response = sg.send(message)
+            print(response.status_code)
+            print(response.body)
+            print(response.headers)
+            return_val = {
+                'status_code' : response.status_code,
+                'ok' : True
+            }
+        except Exception as e:
+            print(e)
+            api.abort(400, 'Email failed to send', ok=False)
+        
+        return return_val
+
+        
+        
+
