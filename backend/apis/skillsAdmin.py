61e78b0196aca2da4de2ed51fcec04acce7ab069
Kevin Cai <kevincai222@gmail.com>
2020-07-01 21:17:27 +1000

    fixed skills admin api route names

--- backend/apis/skillsAdmin.py
+++ backend/apis/skillsAdmin.py
@@ -12,6 +12,7 @@ update_details = api.model('update', {
 
 @api.route('/<string:email>')
 class SkillsAdminAccount(Resource):
+    @api.doc(description="Get information for administrator with email")
     def get(self, email):
         conn = db.get_conn()
         c = conn.cursor()
@@ -36,7 +37,31 @@ class SkillsAdminAccount(Resource):
         }
         return return_val
 
-@api.route('/<string:email>/new')
+    @api.doc(description="Update admin password")
+    @api.expect(update_details)
+    def put(self, email):
+        req =request.get_json()
+
+        conn = db.get_conn()
+        c = conn.cursor()
+        c.execute("SELECT password FROM SkillsBackpackAdmin WHERE email = ?", (email,))
+        query = c.fetchone()
+        if query == None:
+            api.abort(400, "User '{}' not found".format(email), ok=False)
+        
+        password = query[0]
+        if req['password'] == password:
+            c.execute("UPDATE SkillsBackpackAdmin SET password = ? WHERE email = ?", (req['new_password'],email,))
+            conn.commit()
+        else:
+            api.abort(400, "Password incorrect", ok=False)
+        conn.close()
+
+        return {
+            'ok' : 'True'
+        }
+
+@api.route('/new/<string:email>')
 @api.doc(params={'email': 'the email of the skillsBackpack admin account'})
 class SkillsAdminInfo(Resource):
     @api.doc(description="Gets if the admin is new or not (needs to change password)")
@@ -60,28 +85,3 @@ class SkillsAdminInfo(Resource):
                 'ok' : 'True',
                 'message' : 'Password needs to be updated'
             }
-
-@api.route('/<string:email>/details')
-class updateAccountP(Resource):
-    @api.expect(update_details)
-    def put(self, email):
-        req =request.get_json()
-
-        conn = db.get_conn()
-        c = conn.cursor()
-        c.execute("SELECT password FROM SkillsBackpackAdmin WHERE email = ?", (email,))
-        query = c.fetchone()
-        if query == None:
-            api.abort(400, "User '{}' not found".format(email), ok=False)
-        
-        password = query[0]
-        if req['password'] == password:
-            c.execute("UPDATE SkillsBackpackAdmin SET password = ? WHERE email = ?", (req['new_password'],email,))
-            conn.commit()
-        else:
-            api.abort(400, "Password incorrect", ok=False)
-        conn.close()
-
-        return {
-            'ok' : 'True'
-        }
