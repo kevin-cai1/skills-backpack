f28ca60110fd01c29de09b55511e7bf28a7b3a7e
Charmaine Leung <charmaineleung8@gmail.com>
2020-07-11 21:14:33 +1000

    employer search bar pretty much works, just need to integrate with API

--- skills-app/src/home-employer.js
+++ skills-app/src/home-employer.js
@@ -1,126 +1,127 @@
 /* eslint-disable no-use-before-define */
 import React from 'react';
-import { TextField, Chip, InputAdornment } from '@material-ui/core';
-import Autocomplete, { createFilterOptions } from '@material-ui/lab/Autocomplete';
-import AccountCircle from '@material-ui/icons/AccountCircle';
+import Autocomplete, {createFilterOptions} from '@material-ui/lab/Autocomplete';
+import {makeStyles, MuiThemeProvider} from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import SearchIcon from '@material-ui/icons/Search';
+import {Button, ButtonGroup} from '@material-ui/core';
+import { theme } from './App.js'
 
 const filter = createFilterOptions();
-
-const chipNames = [
-    {name: 'css'},
-    {name: 'html'},
-    {name: 'reactjs'},
-    {name: 'python'},
-    {name: 'communication'},
-]
 
 class Home_Employer extends React.Component {
     constructor(props) {
         super(props);
+        const useStyles = makeStyles((theme) => ({
+            root: {
+                width: 500,
+                '& > * + *': {
+                    marginTop: theme.spacing(3),
+                }
+            },
+        }));
         this.state = {
             value: null,
             setValue: null,
-            addedSkills: [],
-            searchValue: '',
+            valueList:'',
         }
-        this.handleDeleteSkill = this.handleDeleteSkill.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
     }
 
-    addSkill(skill) {
-        this.state.addedSkills.push(skill);
-        this.state.searchValue = '';
-        this.state.value = null;
-        this.state.newValue = null;
-        this.forceUpdate();
-    }
-
-    handleDeleteSkill(name) {
-        var tempSkills = [...this.state.addedSkills];
-        var index = tempSkills.indexOf(name);
-        if (tempSkills !== -1) {
-            tempSkills.splice(index, 1);
-            this.setState({addedSkills: tempSkills});
-        }
-        this.forceUpdate();
+    handleSearch() {
+        console.log(this.state.valueList);
     }
 
     render () {
         return (
             <body className="column-container">
+            <div style={{'padding-top':'50px','overflow':'hidden'}}>
+                <div style={{'float':'right', 'marginRight':'90px'}}>
+                    <MuiThemeProvider theme={theme}>
+                        <ButtonGroup variant="contained"
+                                     aria-label="contained primary button group">
+                            <Button href='./my-profile'>
+                                My Profile
+                            </Button>
+                        </ButtonGroup>
+                    </MuiThemeProvider>
+                </div>
+            </div>
             <div className="center-align-container">
-                <div style={{'display': 'inline-block','padding-top':'100px'}}>
+                <div style={{'display': 'inline-block','padding-top':'40px'}}>
                     <h3>Candidate Search</h3>
-                    <Autocomplete
-                        value={this.state.value}
-                        onChange={(event, newValue) => {
-                            if (typeof newValue === 'string') {
-                                this.state.setValue({
-                                    name: newValue,
-                                });
-                            } else if (newValue && newValue.inputValue) {
-                                // Create a new value from the user input
-                                this.state.setValue({
-                                    name: newValue.inputValue,
-                                });
-                            } else {
-                                this.state.setValue = newValue;
-                                this.addSkill(newValue);
-                            }
-                        }}
-                        filterOptions={(options, params) => {
-                            const filtered = filter(options, params);
+                    <div style={{
+                        width: 500,
+                        '& > * + *': {
+                        marginTop: '3px',
+                    },
+                    }}>
+                        <Autocomplete
+                            multiple
+                            id="tags-outlined"
+                            options={chipNames}
+                            getOptionLabel={(option) => option.name}
+                            filterSelectedOptions
+                            onChange={(event, newValue) => {
+                                if (typeof newValue === 'string') {
+                                    this.state.setValue({
+                                        name: newValue,
+                                    });
+                                } else if (newValue && newValue.inputValue) {
+                                    // Create a new value from the user input
+                                    this.state.setValue({
+                                        name: newValue.inputValue,
+                                    });
+                                } else {
+                                    this.state.setValue = newValue;
+                                }
+                                this.state.valueList = newValue;
+                            }}
+                            filterOptions={(options, params) => {
+                                const filtered = filter(options, params);
 
-                            // Suggest the creation of a new value
-                            if (params.inputValue !== '') {
-                                filtered.push({
-                                    inputValue: params.inputValue,
-                                    name: `Add "${params.inputValue}"`,
-                                });
-                            }
+                                // Suggest the creation of a new value
+                                if (params.inputValue !== '') {
+                                    filtered.push({
+                                        inputValue: params.inputValue,
+                                        name: `Add "${params.inputValue}"`,
+                                    });
+                                }
 
-                            return filtered;
-                        }}
-                        selectOnFocus
-                        clearOnBlur
-                        handleHomeEndKeys
-                        id="free-solo-with-text-demo"
-                        options={chipNames}
-                        getOptionLabel={(option) => {
-                            // Value selected with enter, right from the input
-                            if (typeof option === 'string') {
-                                return option;
-                            }
-                            // Add "xxx" option created dynamically
-                            if (option.inputValue) {
-                                return option.inputValue;
-                            }
-                            // Regular option
-                            return option.name;
-                        }}
-                        renderOption={(option) => option.name}
-                        style={{ width: 600, height: 400 }}
-                        freeSolo
-                        renderInput={(params) => (
-                            <TextField
-                                {...params}
-                               label="Search.."
-                                color="primary"
-                                variant="outlined"
-                                value={this.state.searchValue}
-                                multiline
-                                rowsMax={3}
-                                InputProps={{
-                                    ...params.InputProps, startAdornment: (
-                                        <InputAdornment position="start">
-                                            { this.state.addedSkills.map(i =>
-                                                <Chip label={i.name} color="primary" onDelete={() => this.handleDeleteSkill(i)} style={{'margin':'0px 2px'}}/>
-                                            )}
-                                         </InputAdornment>
-                                    ),
-                                }}
-                            />
-                        )}
-                    />
+                                return filtered;
+                            }}
+                            getOptionLabel={(option) => {
+                                // Value selected with enter, right from the input
+                                if (typeof option === 'string') {
+                                    return option;
+                                }
+                                // Add "xxx" option created dynamically
+                                if (option.inputValue) {
+                                    return option.inputValue;
+                                }
+                                // Regular option
+                                return option.name;
+                            }}
+                            renderOption={(option) => option.name}
+                            renderInput={(params) => (
+                                <div className="row-container">
+                                    <TextField
+                                        {...params}
+                                        variant="outlined"
+                                        label="Enter some skills.."
+                                    />
+                                    <MuiThemeProvider theme={theme}>
+                                        <ButtonGroup variant="contained" color="primary"
+                                                     aria-label="contained primary button group">
+                                            <Button onClick={this.handleSearch} variant="contained" color="primary" style={{'minWidth':'73px'}}>
+                                                <SearchIcon/>
+                                            </Button>
+                                        </ButtonGroup>
+                                    </MuiThemeProvider>
+                                </div>
+                            )}
+                        />
+                    </div>
                 </div>
             </div>
             </body>
@@ -129,3 +130,11 @@ class Home_Employer extends React.Component {
 }
 
 export default Home_Employer;
+
+const chipNames = [
+    {name: 'css'},
+    {name: 'html'},
+    {name: 'reactjs'},
+    {name: 'python'},
+    {name: 'communication'},
+]
